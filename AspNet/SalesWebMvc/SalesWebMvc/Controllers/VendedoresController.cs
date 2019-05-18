138c3749d33aeb316dcca78c677621f7b8cadecc
Gilberto <saviskydeveloper@gmail.com>
2019-05-18 10:59:50 -0300

    SellerService e FindAll sincrono

--- AspNet/SalesWebMvc/SalesWebMvc/Controllers/VendedoresController.cs
+++ AspNet/SalesWebMvc/SalesWebMvc/Controllers/VendedoresController.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Services;
 
 namespace SalesWebMvc.Controllers
 {
     public class VendedoresController : Controller
     {
+        private readonly VendedoresService _vendedoresService;
+
+        public VendedoresController(VendedoresService vendedoresService)
+        {
+            _vendedoresService = vendedoresService;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var lista = _vendedoresService.FindAll();
+            return View(lista);
         }
     }
 }
---
+++ AspNet/SalesWebMvc/SalesWebMvc/Services/VendedoresService.cs
@@ -0,0 +1,23 @@
+﻿using SalesWebMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Services
+{
+    public class VendedoresService
+    {
+        private readonly SalesWebMvcContext _context;
+
+        public VendedoresService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public List<Vendedor> FindAll()
+        {
+            return _context.Vendedor.ToList();
+        }
+    }
+}
